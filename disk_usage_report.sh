ddfb2472c61a6c1946e3861e687e4bbd7f5de802
Hudson Marques <hudsonmq1@gmail.com>
2025-06-07 07:46:09 +0000

    fix: agrupa redireções para eliminar SC2129

--- disk_usage_report.sh
+++ disk_usage_report.sh
@@ -4,10 +4,8 @@
 LOGFILE="$HOME/disk_usage.log"
 
 # Cabeçalho com data/hora
-echo "===== $(date '+%Y-%m-%d %H:%M:%S') =====" >> "$LOGFILE"
-
-# Exibe uso de disco e anexa ao log
-df -h >> "$LOGFILE"
-
-# Linha em branco para organizar
-echo "" >> "$LOGFILE"
+{
+echo "===== $(date '+%Y-%m-%d %H:%M:%S') ====="
+df -h
+echo ""
+} >> "$LOGFILE"
